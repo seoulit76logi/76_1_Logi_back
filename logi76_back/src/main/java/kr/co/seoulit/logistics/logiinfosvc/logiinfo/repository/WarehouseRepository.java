c22411481894e26bbd2a39a3c6c051d3d99eb18c
davinc1001 <127849297+davinc1001@users.noreply.github.com>
2023-08-11 17:03:53 +0900

    Delete logi76_back directory

--- logi76_back/src/main/java/kr/co/seoulit/logistics/logiinfosvc/logiinfo/repository/WarehouseRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.seoulit.logistics.logiinfosvc.logiinfo.repository;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import kr.co.seoulit.logistics.logiinfosvc.logiinfo.to.WarehouseTO;
-
-public interface WarehouseRepository  extends CrudRepository<WarehouseTO, String> {
-	List<WarehouseTO> findAll();
-}
